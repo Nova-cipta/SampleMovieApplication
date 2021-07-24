21163d797090769150a83a3047a4b9eae1a803f1
Nova <nova.cipta26@gmail.com>
2021-07-24 13:08:49 +0800

    completely removing unused module

--- app/dynamicfeature/favorite/src/main/java/com/madesubs/favorite/FavoriteViewModel.kt
+++
@@ -1,9 +0,0 @@
-package com.madesubs.favorite
-
-import androidx.lifecycle.ViewModel
-import androidx.lifecycle.asLiveData
-import com.madesubs.core.domain.usecase.MovieUseCase
-
-class FavoriteViewModel(useCase: MovieUseCase) : ViewModel() {
-    val favoriteMovie = useCase.getFavoriteMovie().asLiveData()
-}
